922e949c932bdfee68d86e528eea09ec7b5a960e
MikeG495 <77942643+MikeG495@users.noreply.github.com>
2021-02-01 15:56:32 -0500

    Delete java.js

--- assignments/Griffin/1/java.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express')
-const { createReadStream } = require('fs')
-const bodyParser = require('body-parser')
-const app = express()
-app.get ('/', (req, res) => { createReadStream('Harrypotter.html').pipe(res)
-})
-app.listen(8080)
