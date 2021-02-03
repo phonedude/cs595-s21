fb4911b404f13b50d8950dbee65da4f82669f34a
lbartels <lbart001@odu.edu>
2021-02-03 14:31:20 -0500

    Add file paths to server.js.

--- assignments/Bartels/1/server.js
+++ assignments/Bartels/1/server.js
@@ -6,8 +6,16 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
+app.get('/Logan.html', (req, res) => {
     createReadStream('Logan.html').pipe(res)
+})
+
+app.get('/Grimm.html', (req, res) => {
+    createReadStream('Grimm.html').pipe(res)
+})
+
+app.get('/Longmire.html', (req, res) => {
+    createReadStream('Longmire.html').pipe(res)
 })
 
 app.listen(4000)
