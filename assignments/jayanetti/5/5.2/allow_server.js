859714f598e9fe1360b5462b928509c9002f9190
Himarsha Jayanetti <53842088+himarshaj@users.noreply.github.com>
2021-04-02 01:38:16 -0400

    separated files for easy demo

---
+++ assignments/jayanetti/5/5.2/allow_server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { createReadStream } = require('fs')
+const app = express()
+const port = 5002
+var cors = require('cors')
+
+app.use(cors())
+app.get('/', (req, res) => {
+  res.set('Access-Control-Expose-Headers', '*') /*X-CS595s21-movie, X-CS595s21-tvshow*/
+  res.set('X-CS595s21-movie', 'Me before you')
+  res.set('X-CS595s21-tvshow', 'Spellbinder')
+  res.set('X-CS595s21-musicband', 'Westlife') 
+  res.send("<html><title>Himarsha's Page</title><h1>Welcome to my page!<br></h1>Himarsha Jayanetti, CS595 - Spring 2021, ODU</html>")
+})
+
+app.listen(port, () => {
+  console.log(`Server 2 is listening at http://localhost:${port}`)
+})
+
+app.use(express.static('files'))
