8c6ce3d6aa4b5c307977d00fe04e8a11d6770af1
phonedude <mln@cs.odu.edu>
2021-04-15 18:38:50 -0400

    fixed CORS demo

--- code/dictionary-5.js
+++ code/dictionary-5.js
@@ -5,9 +5,15 @@ const app = express()
 
 const COMMAND = 'open /Applications/Dictionary.app'
 
-app.get('/', (req, res) => {
+app.options('/', (req, res) => {
+	res.set('Access-Control-Allow-Origin', 'http://attacker.com:4001')
+	res.set('Access-Control-Allow-Methods', 'PUT')
+	res.send('ok')
+})
+
+app.put('/', (req, res) => {
   exec(COMMAND, err => {
-    // res.set('Access-Control-Allow-Origin', '*')
+    res.set('Access-Control-Allow-Origin', 'http://attacker.com:4001')
     if (err) res.status(500).send(err)
     else res.status(200).send('Success')
   })
